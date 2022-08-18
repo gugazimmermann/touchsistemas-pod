7cb34001960a0cff9b44b8c44e611e757438d821
Guga Zimmermann <gugazimmermann@gmail.com>
2022-08-18 16:49:15 -0300

    api call

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
@@ -13,9 +12,7 @@ function App() {
 	const [event, setEvent] = useState();
 	const [logo, setLogo] = useState(null);
 	const [phone, setPhone] = useState('');
-	const [code, setCode] = useState('');
 	const [visitor, setVisitor] = useState(undefined);
-	const [final, setFinal] = useState(undefined);
 	const [error, setError] = useState(false);
 
 	function normalizePhone(value) {
@@ -42,43 +39,15 @@ function App() {
 			return;
 		}
 		try {
-			const res = await axios.post(`${process.env.REACT_APP_API}events/visitors`, {
+			const res = await axios.post(`${process.env.REACT_APP_API}events/pod/confirm`, {
 				eventID: params.id,
 				phone: `+55${phone.replace(/\D/g, '')}`,
 			});
 			if (res && res.status === 200) {
-				setVisitor(res.data)
-			} else {
-				setError(true);
+				console.log(res.data)
+				setVisitor(res.data);
 			}
-		} catch (err) {
-			setError(true);
-		}
-		setLoading(false);
-	}
-
-	async function handleSubmitCode(e) {
-		e.preventDefault();
-		setError(false);
-		setLoading(true);
-		if (!code || code.length < 6 || +code !== +visitor.code){
-			console.log(code)
-			setError(true);
-			setLoading(false);
-			return;
-		}
-		try {
-			const res = await axios.post(`${process.env.REACT_APP_API}events/confirm`, {
-				visitorID: visitor.id,
-				eventID: params.id,
-				phone: visitor.phone,
-				code
-			});
-			if (res && res.status === 200) {
-				setFinal(res.data)
-			} else {
-				setError(true);
-			}
+			else setError(true);
 		} catch (err) {
 			setError(true);
 		}
@@ -109,11 +78,6 @@ function App() {
 		return false;
 	}
 
-	function isCodeDisabled() {
-		if (code.length < 6) return true;
-		return false;
-	}
-
 	useEffect(() => {
 		if (params.id) {
 			handleEvent();
@@ -125,7 +89,7 @@ function App() {
 	function renderPhone() {
 		return (
 			<>
-				<h2 className="text-2xl text-center">Digite seu Celular</h2>
+				<h2 className="text-2xl text-center">Digite o Celular</h2>
 				<form onSubmit={(e) => handleSubmit(e)} className="w-full flex justify-end items-center relative">
 					<input
 						value={phone || ''}
@@ -139,62 +103,38 @@ function App() {
 						<i className={`bx bxs-phone-outgoing  text-4xl ${error ? 'text-red-500' : 'text-secondary'}`} />
 					</button>
 				</form>
-				<p className="text-xl text-center">E retire seu copo gratuitamente!</p>
 			</>
 		);
 	}
 
-	function renderCode() {
+	function renderVisitor() {
 		return (
 			<>
-				<h2 className="text-2xl text-center">Código de Verificação</h2>
-				<form onSubmit={(e) => handleSubmitCode(e)} className="w-full flex justify-end items-center relative">
-					<input
-						value={code || ''}
-						onChange={(e) => setCode(e.target.value)}
-						type="number"
-						className={`w-full p-3 text-xl font-bold bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:border-amber-500 focus:outline-none ${
-							error ? 'border-red-500' : 'border-amber-300'
-						}`}
-					/>
-					<button type="submit" className="absolute right-2" disabled={isCodeDisabled()}>
-						<i className={`bx bxs-right-top-arrow-circle text-4xl ${error ? 'text-red-500' : 'text-secondary'}`} />
-					</button>
-				</form>
-				<p className="text-xl text-center">
-					{/* Participe de nossa pesquisa ao final do evento e concora a um kit cervejeiro! */}
-					Você recebeu seu código por SMS no número cadastrado.
-				</p>
-			</>
-		);
-	}
-
-	function renderFinal() {
-		return (
-			<>
-				<h2 className="text-2xl text-center">Obrigado pela sua participação!</h2>
-				<p className="text-xl text-center">
-					Retire seu copo gratuitamente em um dos pontos identificados.
-				</p>
-				<p className="text-xl text-center">
-					Participe de nossa pesquisa ao final do evento e concora a um kit cervejeiro!
-				</p>
+				<p className="text-2xl text-center">{phone}</p>
+				<p className="text-2xl text-center">Confirmado: {!visitor.confirmation ? 'Não' : 'Sim'}</p>
+				<p className="text-2xl text-center">Item Retirado: {!visitor.codeUsed ? 'Não' : 'Sim'}</p>
+				<button type='button' className='bg-secondary px-4 py-2 rounded text-white font-bold shadow-lg'>Confirmar Retirada</button>
 			</>
 		);
 	}
 
 	return (
 		<>
-			<Nav />
+			<Nav eventID={params.id}  />
 			<div className="h-screen flex flex-row justify-center items-center">
 				{loading && <Loading />}
 				{!loading && event && (
 					<div className="bg-white w-full mx-4 sm:w-10/12 md:w-6/12 sm:mx-0 p-8 sm:p-8 flex flex-col justify-center items-center gap-4 rounded-lg shadow-lg">
-						{logo && <img alt="logo" className="object-scale-down w-6/12 rounded-md" src={`${process.env.REACT_APP_LOGO_BUCKET}${logo}`} />}
+						{logo && (
+							<img
+								alt="logo"
+								className="object-scale-down w-6/12 rounded-md"
+								src={`${process.env.REACT_APP_LOGO_BUCKET}${logo}`}
+							/>
+						)}
 						<h1 className="font-bold text-2xl text-center">{event.name}</h1>
-						{!visitor && !final && renderPhone()}
-						{visitor && !final && renderCode()}
-						{visitor && final && renderFinal()}
+						{!visitor && renderPhone()}
+						{visitor && renderVisitor()}
 					</div>
 				)}
 			</div>
